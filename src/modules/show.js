36a2ebc876bacdfe9c22b37f5fae63d2e813e343
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-19 19:05:16 +0000

    Add function to show score on the webpage

--- src/modules/show.js
+++ src/modules/show.js
@@ -1,20 +1,17 @@
-import displayGame from './display.js';
-import add from './add.js';
+import addScores from './add.js';
 
-/*
- The function listens for a form submission, adds the name and score values to a list, displays the
- list, and resets the form. */
-const showScores = () => {
-  const form = document.querySelector('form');
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const name = document.querySelector('#name');
-    const score = document.querySelector('#score');
-    add(name.value, score.value);
-    displayGame();
-    name.value = '';
-    score.value = '';
+// This function displays sorted scores in a list format on a webpage.
+const display = async () => {
+  const displayContainer = document.querySelector('.scores');
+  const sortedScores = await addScores();
+  displayContainer.innerHTML = '';
+  sortedScores.forEach((singleGame, index) => {
+    const li = document.createElement('li');
+    li.textContent = `${singleGame.user} : ${singleGame.score}`;
+    displayContainer.appendChild(li);
+    li.style.backgroundColor = index % 2 === 0 ? '#f2f2f2' : '#fff';
+    displayContainer.appendChild(li);
   });
 };
 
-export default showScores;
+export default display;
