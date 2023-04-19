5738cd8c3dea08a0175ecf623b16de4bd984f3e2
Collins Bawa <collinskwamebawa@yahoo.com>
2023-04-19 19:06:05 +0000

    Add event listener for the refresh button

--- src/script.js
+++ src/script.js
@@ -1,17 +1,39 @@
-// These lines of code are importing modules and stylesheets into the JavaScript file.
 import './style.css';
-import displayGame from './modules/display.js';
-import showScores from './modules/show.js';
+/* These lines of code are importing two functions `displayScore` and `display` from two different
+modules `./modules/display.js` and `./modules/show.js` respectively. These functions are then used
+in the event listeners attached to the form submit button and the refresh button to display and
+update the scores on the webpage. */
+import displayScore from './modules/display.js';
+import display from './modules/show.js';
 
-/* These lines of code are selecting the HTML element with the ID "button" and adding
-an event listener to it. When the button is clicked, the `showScores` function will be executed. */
+const form = document.querySelector('form');
+
+/* This code is adding an event listener to the form element that listens for a submit event.
+When the form is submitted, the event listener function is executed. The function first prevents
+the default form submission behavior using `event.preventDefault()`. It then retrieves the values
+of the name and score inputs from the form, and passes them as arguments to the `displayScore`
+function, which is imported from the `./modules/display.js` module. The `await` keyword is used
+to wait for the `displayScore` function to complete before moving on to the next line of code.
+After the score is displayed, the function clears the input fields by setting their values
+to an empty string. Finally, it calls the `display` function, which is imported from the
+`./modules/show.js` module, to update the scores displayed on the webpage. */
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const nameInput = document.querySelector('#name');
+  const scoreInput = document.querySelector('#score');
+  const name = nameInput.value;
+  const score = scoreInput.value;
+  await displayScore(name, score);
+  nameInput.value = '';
+  scoreInput.value = '';
+  await display();
+});
+
 const button = document.querySelector('#button');
-button.addEventListener('click', showScores);
+button.addEventListener('click', display);
 
-/* is adding an event listener to the window object. When the window has finished loading, the
-`displayGame` function will be executed. This ensures that the `displayGame` function is only
-called after all the resources on the page have finished loading, which can prevent errors and
-improve performance. */
-window.addEventListener('load', () => {
-  displayGame();
+const refreshBtn = document.querySelector('.refresh');
+
+refreshBtn.addEventListener('click', async () => {
+  await display();
 });
